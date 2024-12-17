ff34525f422305a0a879279519b453dfe33dbb98
adarshsrivastav375 <adarshsrivastav.375@gmail.com>
2024-12-17 18:25:57 +0530

    favorite controllers

--- src/routes/deviceId.routes.js
+++ src/routes/deviceId.routes.js
@@ -1,9 +1,13 @@
-import { createOrUpdateDevice } from "../controller/deviceId.controller.js";
+import { createOrUpdateDevice,addFavoriteProperty } from "../controller/deviceId.controller.js";
 import express from "express";
 
 const deviceRouter = express.Router();
 deviceRouter.post("/add-update", createOrUpdateDevice)
 
-
+/*--------------------------------------------------favorites------------------------------------*/
+deviceRouter.post("/add-to-favorites", createOrUpdateDevice)
+deviceRouter.post("/remove-from-favorites", addFavoriteProperty)
+deviceRouter.post("/add-update", createOrUpdateDevice)
+deviceRouter.post("/add-update", createOrUpdateDevice)
 
 export default deviceRouter;

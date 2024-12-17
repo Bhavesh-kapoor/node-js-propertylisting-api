b33fa2e726f50528fe1ab76f05de6009c546d576
Rishabh gupta <guptarishabh792@gmail.com>
2024-12-17 16:40:34 +0530

    update route

--- src/routes/property.routes.js
+++ src/routes/property.routes.js
@@ -1,18 +1,34 @@
-import express from 'express';
-import { createProperty, updateProperty, getProperties, getProperty, deleteProperty, propertyValidator } from '../controller/property.controller.js';
-import { multerUpload } from '../middlewere/multer.middlewere.js';
+import express from "express";
+import {
+  createProperty,
+  updateProperty,
+  getProperties,
+  getProperty,
+  deleteProperty,
+  propertyValidator,
+} from "../controller/property.controller.js";
+import { multerUpload } from "../middlewere/multer.middlewere.js";
+
 const propertyRouter = express.Router();
 
 const multiUpload = multerUpload.fields([
-    { name: "imagefiles", maxCount: 10 },
-    { name: "videofiles", maxCount: 2 }
-])
+  { name: "imagefiles", maxCount: 10 },
+  { name: "videofiles", maxCount: 2 },
+]);
 
 propertyRouter.post("/add", multiUpload, propertyValidator, createProperty);
 
-propertyRouter.put("/update/:id", multiUpload, propertyValidator, updateProperty);
-propertyRouter.get("/get-list", getProperties)
-propertyRouter.get("/get/:id", getProperty)
-propertyRouter.delete("/delete-property", deleteProperty)
+propertyRouter.put(
+  "/update/:id",
+  multiUpload,
+  propertyValidator,
+  updateProperty
+);
 
-export default propertyRouter; 
+propertyRouter.get("/get-list", getProperties);
+
+propertyRouter.get("/get/:id", getProperty);
+
+propertyRouter.delete("/delete-property/:id", deleteProperty);
+
+export default propertyRouter;

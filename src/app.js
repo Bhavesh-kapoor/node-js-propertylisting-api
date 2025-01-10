2e1052f6841e8cb3dc10d75b78bee0d03a1f675f
Adarsh Srivastav <adarshsrivastav375@gmail.com>
2025-01-10 15:03:55 +0530

    cors fixed for specific url

--- src/app.js
+++ src/app.js
@@ -5,10 +5,9 @@ import colors from "colors";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import { logger } from "./config/logger.js";
-import path from 'path'
-import { fileURLToPath } from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 import scheduleCronJobs from "./jobs/subscriptionJob.js";
-
 
 dotenv.config();
 const app = express();
@@ -29,7 +28,17 @@ app.use(express.static(path.join(__dirname)));
 // Middleware
 app.use(helmet());
 app.use(cookieParser());
-app.use(cors());
+const allowedOrigins = process.env.ALLOWED_ORIGINS;
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1) callback(null, true);
+    else callback(new Error("Not allowed by CORS"));
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
 // app.use(cors(corsOptions));
 
 // Middleware for parsing JSON and URL-encoded bodies
@@ -37,27 +46,28 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-
 // Logging Middleware
 app.use((req, res, next) => {
-    const startTime = process.hrtime();
-    res.on("finish", () => {
-        const fetchStatus = () => {
-            if (res.statusCode >= 500) return colors.red(`${res.statusCode}`);
-            else if (res.statusCode >= 400) return colors.yellow(`${res.statusCode}`);
-            else if (res.statusCode >= 300) return colors.cyan(`${res.statusCode}`);
-            else if (res.statusCode >= 200) return colors.green(`${res.statusCode}`);
-            else return colors.white(`${res.statusCode}`);
-        };
-        const diff = process.hrtime(startTime);
-        const responseTime = (diff[0] * 1e3 + diff[1] * 1e-6).toFixed(2);
-        logger.info(
-            `${"METHOD:".blue} ${req.method.yellow} - ${"URL:".blue} ${req.originalUrl.yellow
-            } - ${"STATUS:".blue} ${fetchStatus()} - ${"Response Time:".blue} ${responseTime.magenta
-            } ${"ms".magenta}`
-        );
-    });
-    next();
+  const startTime = process.hrtime();
+  res.on("finish", () => {
+    const fetchStatus = () => {
+      if (res.statusCode >= 500) return colors.red(`${res.statusCode}`);
+      else if (res.statusCode >= 400) return colors.yellow(`${res.statusCode}`);
+      else if (res.statusCode >= 300) return colors.cyan(`${res.statusCode}`);
+      else if (res.statusCode >= 200) return colors.green(`${res.statusCode}`);
+      else return colors.white(`${res.statusCode}`);
+    };
+    const diff = process.hrtime(startTime);
+    const responseTime = (diff[0] * 1e3 + diff[1] * 1e-6).toFixed(2);
+    logger.info(
+      `${"METHOD:".blue} ${req.method.yellow} - ${"URL:".blue} ${
+        req.originalUrl.yellow
+      } - ${"STATUS:".blue} ${fetchStatus()} - ${"Response Time:".blue} ${
+        responseTime.magenta
+      } ${"ms".magenta}`
+    );
+  });
+  next();
 });
 
 /*----------------------------------------------Routes------------------------------------------*/
@@ -65,10 +75,10 @@ import integratedRoutes from "./routes/integrated.routes.js";
 app.use("/api", integratedRoutes);
 
 /*----------------------------------------------cronjob------------------------------------------*/
-scheduleCronJobs()
+scheduleCronJobs();
 
 app.use((request, response) => {
-    response.status(404).json({ success: false, message: "Route Not Found" });
+  response.status(404).json({ success: false, message: "Route Not Found" });
 });
 
 export { app };
